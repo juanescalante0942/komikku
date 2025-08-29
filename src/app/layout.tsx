ebba6bf6658f8d1e5602df2029aa7821d4f18a5c
juanescalante0942 <osiris0942@gmail.com>
2025-08-29 14:49:23 +0800

    Favicon ico formatting  fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata = {
   description:
     "Discover and read your favorite manga online. Browse by genre, author, and popularity.",
   icons: {
-    icon: [{ url: "/favicon.svg", type: "image/svg+xml" }],
+    icon: "/favicon.ico",
   },
 };
 
